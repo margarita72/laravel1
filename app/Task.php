b1d6a53868b7703e7f98c3b7d82d207df5ba32f4
Margo <rita.yurtina@mail.ru>
2018-11-08 22:35:05 +0500

    mya commit4

--- app/Task.php
+++ app/Task.php
@@ -8,6 +8,6 @@ class Task extends Model
 {
     //
     public static function incomplete(){
-    	return static::where('completed', 0)->get();
+    	return static::where('completed', 0)->get(); //модель описывает вывод на экран тех строк что удовлетворяют условию поле completed равен 0, т.е. false
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,18 @@
 |
 */
 
-Route::get('tasks', function () {
-	//$tasks = DB::table('tasks')->get();
-	//$tasks = App\Task::all();
-	$tasks = App\Task::incomplete();
-    return view('tasks.index',compact('tasks'));
-});
-Route::get('/tasks/{tas}', function ($id) {
-	//$task = DB::table('tasks')->find($id);
-	$task = App\Task::find($id);
-    return view('tasks.show',compact('task'));
-});
+Route::get('tasks', 'TasksController@index');//после @ название метода, а до название контроллера
+Route::get('tasks/{task}', 'TasksController@show');
+
+//Route::get('tasks', function () {//
+//	//$tasks = DB::table('tasks')->get(); //выводит// из таблицы данные, лучше не //использовать такую запись, а описывать в модели//
+//	//$tasks = App\Task::all(); //обеспечивает вывод на экран всех полей//
+//	$tasks = App\Task::incomplete(); //переменная выполняет задание по выводу данных //описанное в модели Task с методом incomplete
+//    return view('tasks.index',compact('tasks'));
+//});
+//Route::get('/tasks/{tas}', function ($id) {
+//	//$task = DB::table('tasks')->find($id);з
+//	$task = App\Task::find($id);
+//    return view('tasks.show',compact('task'));
+//});
+

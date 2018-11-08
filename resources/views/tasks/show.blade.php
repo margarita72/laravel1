cc2875a6a1d638c398cd5788c5c8e032d45f0c1a
Margo <rita.yurtina@mail.ru>
2018-11-08 20:41:44 +0500

    first commit

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    {{-- Вывод данных по id одного конкретного поля--}}
+       
+            <h1>{{$task->name}}</h1>
+    
+       
+    
+</body>
+</html>
